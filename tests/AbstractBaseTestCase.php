ac9ff40807473eef605a9f60fabb5d9755229004
Hugo Rafael Azevedo <bitbucket@hradigital.com>
2019-05-01 15:16:43 +0100

    Added first draft for the test's configurations.

---
+++ tests/AbstractBaseTestCase.php
@@ -0,0 +1,21 @@
+<?php
+namespace Hradigital\Tests\Datatypes;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Abstract Base Test Case class.
+ *
+ * All tests should extend from this base class.
+ *
+ * All shared testing functionality and/or configuration should be added to this file.
+ *
+ * @package   Hradigital\Datatypes
+ * @copyright Hugo Rafael Azevedo <github@hradigital.com>
+ * @author    Hugo Rafael Azevedo <github@hradigital.com>
+ * @license   MIT
+ * @since     1.0.0
+ */
+abstract class AbstractBaseTestCase extends TestCase
+{
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * PHPUnit testing autoload file.
+ *
+ * For Unit testing only, please link PHPUnit to this file. This file will autoload Composer's dependencies.
+ *
+ * @package   Hradigital\Datatypes
+ * @copyright Hugo Rafael Azevedo <github@hradigital.com>
+ * @author    Hugo Rafael Azevedo <github@hradigital.com>
+ * @license   MIT
+ * @since     1.0.0
+ */
+
+// Autoload Composer's dependencies, and Aesir-Base's namespace.
+require __DIR__ . '/../vendor/autoload.php';
