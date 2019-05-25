f306f6d2248c5ec3ca1c9547e9f97f46fcf6b27b
Hugo Rafael Azevedo <bitbucket@hradigital.com>
2019-05-25 14:01:17 +0100

    Added toBoolean() shared method.

--- src/Scalar/AbstractWriteInteger.php
+++ src/Scalar/AbstractWriteInteger.php
@@ -65,4 +65,18 @@ abstract class AbstractWriteInteger extends AbstractReadInteger
     {
         return \intval($this->value / $value);
     }
+
+    /**
+     * Converts float's instance to an equivalent boolean instance.
+     *
+     * A <i>ReadonlyBoolean</i> will be returned, as Booleans don't require Immutable and Mutable
+     * distinctions.
+     *
+     * @since  1.0.0
+     * @return ReadonlyBoolean
+     */
+    public function toBoolean(): ReadonlyBoolean
+    {
+        return ReadonlyBoolean::fromInteger($this->value);
+    }
 }
