52e1ef3c28711c1f4c1250e66ae588a8a44ac745
Hugo Rafael Azevedo <h.azevedo@exporo.com>
2022-02-28 17:50:42 +0000

    fix: Fix namespace

--- tests/AbstractBaseTestCase.php
+++ tests/AbstractBaseTestCase.php
@@ -1,6 +1,8 @@
-<?php declare(strict_types=1);
+<?php
 
-namespace Hradigital\Tests\Datatypes;
+declare(strict_types=1);
+
+namespace HraDigital\Tests\Datatypes;
 
 use PHPUnit\Framework\TestCase;
 
