ab125300cd96ee84cc2796524a51972149a23a47
Hugo Rafael Azevedo <h.azevedo@exporo.com>
2022-03-05 18:53:52 +0000

    refactor: Remove type hinting

--- src/Datetime/DateTimeInterface.php
+++ src/Datetime/DateTimeInterface.php
@@ -14,26 +14,17 @@ namespace HraDigital\Datatypes\Datetime;
  */
 interface DateTimeInterface
 {
-    const string ATOM = \DateTimeInterface::ATOM;
-    const string COOKIE = \DateTimeInterface::COOKIE;
-    const string ISO8601 = \DateTimeInterface::ISO8601;
-    const string RFC822 = \DateTimeInterface::RFC822;
-    const string RFC850 = \DateTimeInterface::RFC850;
-    const string RFC1036 = \DateTimeInterface::RFC1036;
-    const string RFC1123 = \DateTimeInterface::RFC1123;
-    const string RFC7231 = \DateTimeInterface::RFC7231;
-    const string RFC2822 = \DateTimeInterface::RFC2822;
-    const string RFC3339 = \DateTimeInterface::RFC3339;
-    const string RFC3339_EXTENDED = \DateTimeInterface::RFC3339_EXTENDED;
-    const string RSS = \DateTimeInterface::RSS;
-    const string W3C = \DateTimeInterface::W3C;
-
-    /* Methods
-    public diff ( DateTimeInterface $targetObject , bool $absolute = false ) : DateInterval
-    public format ( string $format ) : string
-    public getOffset ( ) : int
-    public getTimestamp ( ) : int
-    public getTimezone ( ) : DateTimeZone|false
-    public __wakeup ( ) : void
-    */
+    const ATOM = \DateTimeInterface::ATOM;
+    const COOKIE = \DateTimeInterface::COOKIE;
+    const ISO8601 = \DateTimeInterface::ISO8601;
+    const RFC822 = \DateTimeInterface::RFC822;
+    const RFC850 = \DateTimeInterface::RFC850;
+    const RFC1036 = \DateTimeInterface::RFC1036;
+    const RFC1123 = \DateTimeInterface::RFC1123;
+    const RFC7231 = \DateTimeInterface::RFC7231;
+    const RFC2822 = \DateTimeInterface::RFC2822;
+    const RFC3339 = \DateTimeInterface::RFC3339;
+    const RFC3339_EXTENDED = \DateTimeInterface::RFC3339_EXTENDED;
+    const RSS = \DateTimeInterface::RSS;
+    const W3C = \DateTimeInterface::W3C;
 }
