551880369f68121799fd9a5420490080f50a838a
Hugo Rafael Azevedo <h.azevedo@exporo.com>
2022-03-09 17:28:39 +0000

    refactor: Improve mass assignment setAttributes() method

--- src/Traits/Entities/CanMassAssignStateTrait.php
+++ src/Traits/Entities/CanMassAssignStateTrait.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace HraDigital\Datatypes\Traits\Entities;
 
+use HraDigital\Datatypes\Exceptions\Datatypes\ParameterOutOfRangeException;
 use HraDigital\Datatypes\ValueObjects\AbstractValueObject;
 
 /**
@@ -20,11 +21,7 @@ trait CanMassAssignStateTrait
      *
      * @param  array $fields - Associative array, where the keys are the class attribute's names.
      *
-     * @throws \UnderflowException       - If supplied array is empty.
-     * @throws \UnexpectedValueException - If supplied array is not a string-key only associative array.
-     * @throws \InvalidArgumentException - If supplied attribute's names are not non empty strings.
-     * @throws \BadMethodCallException   - If any setter method doesn't exist for a chosen attribute.
-     *
+     * @throws ParameterOutOfRangeException - If supplied array is empty.
      * @return void
      */
     final public function setAttributes(array $fields): void
@@ -32,12 +29,7 @@ trait CanMassAssignStateTrait
         // Validates the supplied array is a non empty associative one.
         $count = \count($fields);
         if ($count === 0) {
-            throw new \UnderflowException('Supplied parameter should be a non empty Array.');
-        }
-
-        // Validates that all the indexes are of String type.
-        if (\count(\array_filter(\array_keys($fields), 'is_string')) !== $count) {
-            throw new \UnexpectedValueException('Supplied parameter should have been an Associative Array.');
+            throw ParameterOutOfRangeException::withName('$fields');
         }
 
         // Process supplied fields.
@@ -48,6 +40,8 @@ trait CanMassAssignStateTrait
         foreach ($mapped as $field => $value) {
             if ($this->{$field} instanceof AbstractValueObject && \method_exists($this->{$field}, 'setAttributes')) {
                 $this->{$field}->setAttributes($value);
+            } elseif (\method_exists($this->{$field}, '__toString')) {
+                $fields[$field] = (string) $value;
             } else {
                 $fields[$field] = $value;
             }
