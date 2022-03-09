f58232886b5799fa751f1550feb2edc2eaa1e04d
Hugo Rafael Azevedo <h.azevedo@exporo.com>
2022-03-09 16:24:28 +0000

    tests: Adapt AbstractValueObject's tests

--- tests/Unit/ValueObjects/TestingNestedValueObject.php
+++ tests/Unit/ValueObjects/TestingNestedValueObject.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace HraDigital\Tests\Datatypes\Unit\ValueObjects;
 
+use HraDigital\Datatypes\Datetime\Datetime;
+use HraDigital\Datatypes\Scalar\Str;
+use HraDigital\Datatypes\Traits\Entities\CanProcessEntityStateTrait;
+use HraDigital\Datatypes\Traits\Entities\CanProcessOnUpdateEventsTrait;
 use HraDigital\Datatypes\Traits\Entities\General\HasActiveTrait;
 use HraDigital\Datatypes\Traits\Entities\General\HasEmailTrait;
 use HraDigital\Datatypes\Traits\Entities\General\HasTitleTrait;
+use HraDigital\Datatypes\Traits\Entities\General\HasUpdatableUpdatedAtTrait;
 use HraDigital\Datatypes\ValueObjects\AbstractValueObject;
 
 /**
@@ -19,8 +24,32 @@ use HraDigital\Datatypes\ValueObjects\AbstractValueObject;
 class TestingNestedValueObject extends AbstractValueObject
 {
     use HasActiveTrait,
-        HasTitleTrait;
+        HasTitleTrait,
+        HasUpdatableUpdatedAtTrait,
+        CanProcessEntityStateTrait,
+        CanProcessOnUpdateEventsTrait;
 
     /** @var array $guarded - List of fields that should not be serializable into JSON. */
     protected array $guarded = ['active'];
+
+    protected function ruleSetUpdatedAtIfMissing(array $fields): array
+    {
+        if (!isset($fields['updated_at'])) {
+            $fields['updated_at'] = (string) Datetime::now()->addHours(-1);
+        }
+
+        return $fields;
+    }
+
+    public function activate(): void
+    {
+        $this->active = true;
+        $this->triggerOnUpdate();
+    }
+
+    public function changeTitle(Str $title): void
+    {
+        $this->title = $title;
+        $this->triggerOnUpdate();
+    }
 }
