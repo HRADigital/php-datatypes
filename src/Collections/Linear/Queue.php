c995057c166bab72a28dda76a7063fa60f85220b
Hugo Rafael Azevedo <h.azevedo@exporo.com>
2022-03-11 17:58:53 +0000

    refactor: improve data handling in class

--- src/Collections/Linear/Queue.php
+++ src/Collections/Linear/Queue.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace HraDigital\Datatypes\Collections\Linear;
 
+use HraDigital\Datatypes\Exceptions\Datatypes\NonEmptyStringException;
+use HraDigital\Datatypes\Scalar\Str;
+
 /**
  * Queue linear Collection.
  *
@@ -36,24 +39,13 @@ class Queue extends AbstractListArray
     /**
      * Initializes an instance of a Queue.
      *
-     * @param array|NULL $initial - Initial element list for the Queue. Can be NULL.
-     *
-     * @throws \InvalidArgumentException - If supplied array is empty, although not NULL.
+     * @param array $initial - Initial element list for the Queue. Defaults to empty array.
      * @return void
      */
-    public function __construct(?array $initial = null)
+    public function __construct(array $initial = [])
     {
-        // Checks initially provided list of elements.
-        if ($initial !== null) {
-            // Validates supplied parameter.
-            if (\count($initial) === 0) {
-                throw new \InvalidArgumentException("Supplied array should be a list of string elements.");
-            }
-
-            // Fills in the Queue.
-            foreach ($initial as $element) {
-                $this->push($element);
-            }
+        foreach ($initial as $element) {
+            $this->push($element);
         }
     }
 
@@ -62,7 +54,7 @@ class Queue extends AbstractListArray
      *
      * @return Queue
      */
-    public function copy(): self
+    public function clone(): Queue
     {
         return new Queue($this->toArray());
     }
@@ -72,9 +64,9 @@ class Queue extends AbstractListArray
      *
      * If the Queue has no elements, it will return NULL.
      *
-     * @return string|NULL
+     * @return Str|NULL
      */
-    public function peek(): ?string
+    public function peek(): ?Str
     {
         // Validates the Queue has elements.
         if ($this->count() === 0) {
@@ -89,9 +81,9 @@ class Queue extends AbstractListArray
      *
      * If the Queue has no elements, it will return NULL.
      *
-     * @return string|NULL
+     * @return Str|NULL
      */
-    public function pop(): ?string
+    public function pop(): ?Str
     {
         return \array_shift($this->list);
     }
@@ -108,10 +100,10 @@ class Queue extends AbstractListArray
     {
         // Validates provided parameter.
         if (\strlen(\trim($element)) === 0) {
-            throw new \InvalidArgumentException("Supplied element must be a non empty string.");
+            throw NonEmptyStringException::withName('$element');
         }
 
         // Adds element to the Queue.
-        $this->list[] = $element;
+        $this->list[] = Str::create($element);
     }
 }
