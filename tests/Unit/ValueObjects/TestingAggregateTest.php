fbdd2a324d92ea081dc4a057ef242d2dc8b9ef26
Hugo Rafael Azevedo <h.azevedo@exporo.com>
2022-03-12 19:47:36 +0000

    tests: add tests for CanSerializeAllToJsonTrait

---
+++ tests/Unit/ValueObjects/TestingAggregateTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HraDigital\Tests\Datatypes\Unit\ValueObjects;
+
+use HraDigital\Datatypes\Datetime\Datetime;
+use HraDigital\Datatypes\Scalar\Str;
+use HraDigital\Tests\Datatypes\AbstractBaseTestCase;
+
+/**
+ * JSON Serializable Aggeregate Unit testing.
+ *
+ * @package   HraDigital\Datatypes
+ * @copyright HraDigital\Datatypes
+ * @license   MIT
+ * @group hugo
+ */
+class TestingAggregateTest extends AbstractBaseTestCase
+{
+    public function testLoadsSuccessfully(): void
+    {
+        $aggregate = new TestingAggregate(
+            Str::create('String Object'),
+            Datetime::fromString('2022-02-02 10:00:00'),
+            new TestingValueObject(
+                TestingValueObject::DATA
+            ),
+            true
+        );
+
+        $array = json_decode(
+            json_encode($aggregate),
+            true
+        );
+
+        // Tests field mapping and onLoad events work.
+        $this->assertIsArray($array);
+        $this->assertArrayHasKey('string', $array);
+        $this->assertArrayHasKey('datetime', $array);
+        $this->assertArrayHasKey('vo', $array);
+        $this->assertArrayHasKey('boolean', $array);
+    }
+}
